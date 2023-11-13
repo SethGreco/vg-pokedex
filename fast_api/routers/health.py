85b04d9d782dd4c36d068c5b25a848958ee5678d
Seth Greco <sgreco@seths-laptop.lan>
2023-11-12 22:09:02 -0600

    ADD: prefix url = /api/v1, docker-compose healthcheck added.

--- fast_api/routers/health.py
+++ fast_api/routers/health.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 
-router = APIRouter(
-    prefix="/health",
-    tags=["health"]
-)
+router = APIRouter(prefix="/health")
 
 
-@router.get("")
+@router.get("", summary="Health Check availabilty")
 async def health():
+    """
+    Health endpoint
+    """
     return {"Status": "Up"}
